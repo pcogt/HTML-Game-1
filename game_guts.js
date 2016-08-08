fe24d33840b9a4fb43e8776ce4cf0a0302eed705
Victoria Hall <pcogt@users.noreply.github.com>
2016-08-07 21:34:58 -0700

    Create game_guts.js
    Put in some initial functions to mess around with the html canvas

---
+++ game_guts.js
@@ -0,0 +1,51 @@
+
+
+
+var myGamePiece;
+
+// Initiates the game
+function startGame() {
+    myGameArea.start();
+    myGamePiece = new component(30, 30, "red", 10, 120);
+}
+
+/****** The functions that create the pieces needed for the game *******/
+
+// creates a rectangle in the game space
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y; 
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    this.update = function(){
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+
+// Creates a canvas on which the game to take place
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20); // updates the canvas every 20ms
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.update();
+}
+
+
