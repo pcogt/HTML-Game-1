42e175a80581cb15150b389155c3b7914f60daa8
Victoria Hall <pcogt@users.noreply.github.com>
2016-08-08 00:27:45 -0700

    Update game_guts.js

--- game_guts.js
+++ game_guts.js
@@ -30,12 +30,12 @@ function component(width, height, color, x, y) {
     }
     this.newPos = function() {
         // don't let the GamePiece leave the canvas
-        if(this.x + this.speedX > 0 && this.x + this.speedX <= defaultCanvasWidth - 10) {
+        if(this.x + this.speedX > 0 && this.x + this.speedX <= defaultCanvasWidth - 30) {
             this.x += this.speedX;
         }
         else {
-            if(this.x + this.speedX > defaultCanvasWidth - 10) {
-                this.x = defaultCanvasWidth - 10;
+            if(this.x + this.speedX > defaultCanvasWidth - 30) {
+                this.x = defaultCanvasWidth - 30;
             }
             else { // this.x + this.speedX <= 0
                 this.x = 0;
@@ -50,12 +50,13 @@ function component(width, height, color, x, y) {
 // Creates a canvas on which the game to take place
 var myGameArea = {
     canvas : document.createElement("canvas"),
+    
     start : function() {
-        this.canvas.width = 440;
+        this.canvas.width = defaultCanvasWidth;
         this.canvas.height = 560;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 10); // updates the canvas every 20ms
+        this.interval = setInterval(updateGameArea, 10); // updates the canvas every 10ms
         window.addEventListener('keydown', function (e) {
             myGameArea.key = e.keyCode;
         })
@@ -63,19 +64,19 @@ var myGameArea = {
             myGameArea.key = false;
         })
     },
+    
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
-function updateGameArea() {
+function updateGameArea() {  // only allow the GamePiece to move left and right on the canvas
     myGameArea.clear();
     myGamePiece.speedX = 0;
-    myGamePiece.speedY = 0; 
+    //myGamePiece.speedY = 0; 
     if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -1; }
     if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 1; }
     
-    // only allow the GamePiece to move left and right on the canvas
     //if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
     //if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
     myGamePiece.newPos(); 
