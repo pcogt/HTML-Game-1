d1e6e777ab1284be1ff84ca19acb2b9ea988301a
Victoria Hall <pcogt@users.noreply.github.com>
2016-08-07 21:57:46 -0700

    Update game_guts.js

--- game_guts.js
+++ game_guts.js
@@ -37,6 +37,12 @@ var myGameArea = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20); // updates the canvas every 20ms
+        window.addEventListener('keydown', function (e) {
+            myGameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.key = false;
+        })
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -45,10 +51,12 @@ var myGameArea = {
 
 function updateGameArea() {
     myGameArea.clear();
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0; 
+    if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -1; }
+    if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 1; }
+    if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
+    if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
     myGamePiece.update();
 }
 
-/****************************
- * *********** STARTS THE GAME
- * *************************/
-startGame();
